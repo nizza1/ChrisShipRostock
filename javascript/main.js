3daa4f185afec2f3f1e7d5425c4d4fb2649bbc42
nizar <nizardjassim@gmail.com>
2024-01-30 11:34:13 +0100

    modal

--- javascript/main.js
+++ javascript/main.js
@@ -202,3 +202,54 @@ function animateOnScroll() {
 
 animateOnScroll();
 //animation About Page 
+
+
+
+
+//carousel
+const carousel = document.querySelector('.carousel-inner');
+const modalContent = document.querySelector('.modal-content');
+const imageSrcs = Array.from(carousel.querySelectorAll('img')).map(img => img.getAttribute('src'));
+
+
+carousel.addEventListener('click', (e) => {
+  console.log('carousel clicked');
+ 
+    // Log the image srcs to the console
+    console.log('Image Srcs:', imageSrcs);
+  
+  
+  $('.bd-example-modal-lg').modal('toggle');
+  createCarousel();
+});
+
+function createCarousel() {
+  modalContent.innerHTML = `
+
+  <div id="carouselExampleIndicators" class="carousel slide carouselCoMo" data-ride="carousel">
+<ol class="carousel-indicators">
+  ${imageSrcs.map((_, index) => `<li data-target="#carouselExampleIndicators" data-slide-to="${index}"${index === 0 ? ' class="active"' : ''}></li>`).join('')}
+</ol>
+<div class="carousel-inner">
+  ${imageSrcs.map((image, index) => `
+    <div class="carousel-item${index === 0 ? ' active' : ''}">
+      <img src="${image}" alt="">
+    </div>`).join('')}
+</div>
+<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+  <span class="sr-only">Previous</span>
+</a>
+<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+  <span class="sr-only">Next</span>
+</a>
+</div>
+
+
+
+  `
+
+}
+
+
